9299fe23bb6a256fa8970c8add5e2ebaefdf7ec0
starrpar <starrparker@live.com>
2025-05-19 22:07:01 -0700

    various updated files

--- rest/src/main/java/com/parker/app/rest/RestApiApplication.java
+++ rest/src/main/java/com/parker/app/rest/RestApiApplication.java
@@ -6,6 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestApiApplication {
 
+	//ToDo: figure out how to use this here to process streaming data
+
+	/*
+	@Bean
+    public Function<KStream<Object, String>, KStream<?, WordCount>> process() {
+
+        return input -> input
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+                .map((key, value) -> new KeyValue<>(value, value))
+                .groupByKey(Serialized.with(Serdes.String(), Serdes.String()))
+                .windowedBy(TimeWindows.of(5000))
+                .count(Materialized.as("word-counts-state-store"))
+                .toStream()
+                .map((key, value) -> new KeyValue<>(key.key(), new WordCount(key.key(), value,
+                        new Date(key.window().start()), new Date(key.window().end()))));
+    }
+	*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
---
+++ test_project/build_docker.cmd
@@ -0,0 +1,7 @@
+Dockerfile - build java .jar file
+Dockerfile - copy .jar file to /app folder and execute (java -jar ...)
+
+docker build . -t local_repo:hr_benefits_agent
+docker images
+docker run -it local_repo:hr_benefits_agent
+
